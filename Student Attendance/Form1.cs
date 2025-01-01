56864782d94f2148ffef94f478efa834415ceb5f
Isko2003 <theazeisko2003@gmail.com>
2025-01-01 12:18:17 +0400

    Add project files.

---
+++ Student Attendance/Form1.cs
@@ -0,0 +1,15 @@
+namespace Student_Attendance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
